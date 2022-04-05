4e7ba3321fe559a0d78b26f0dd296017c65f4d4c
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-04-05 12:52:16 -0500

    Managing Multiple Tracks

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {AppRegistry, LogBox} from 'react-native';
+import {AppRegistry, LogBox, Platform} from 'react-native';
 import 'react-native-gesture-handler';
 import codePush from 'react-native-code-push';
 
@@ -7,6 +7,21 @@ import {name as appName} from './app.json';
 
 LogBox.ignoreLogs(['[react-native-gesture-handler]']);
 
-const CodePushifiedApp = codePush(App);
+const codePushKeys = Platform.select({
+  ios: {
+    STAGING: '_0hL5bn-x0H9VAsAorYFJr4fhqNWQZK-J0Ogu',
+    PRODUCTION: 'UPfjI8kjCtrXI-DCTVy3Wo-yS4mK1jfn6h0oW',
+  },
+  android: {
+    STAGING: 'kA-jV6G-UhSp9z2b1SNbTvLdiEOoRSlih2WpE',
+    PRODUCTION: 'kA-jV6G-UhSp9z2b1SNbTvLdiEOoRSlih2WpE',
+  },
+});
+
+const isBetaUser = false;
+
+const CodePushifiedApp = codePush({
+  deploymentKey: isBetaUser ? codePushKeys.STAGING : codePushKeys.PRODUCTION,
+})(App);
 
 AppRegistry.registerComponent(appName, () => CodePushifiedApp);
